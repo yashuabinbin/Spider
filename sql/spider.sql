77b5aea41807d712d79a21efba91be165cef2244
yashuabinbin <lubingbin@qq.com>
2017-04-25 21:41:34 +0800

    添加了sql目录 及 表sql

---
+++ sql/spider.sql
@@ -0,0 +1,57 @@
+/*
+Navicat MySQL Data Transfer
+
+Source Server         : localhost
+Source Server Version : 50528
+Source Host           : localhost:3306
+Source Database       : spider
+
+Target Server Type    : MYSQL
+Target Server Version : 50528
+File Encoding         : 65001
+
+Date: 2017-04-25 21:40:46
+*/
+
+SET FOREIGN_KEY_CHECKS=0;
+
+-- ----------------------------
+-- Table structure for `sp_seed`
+-- ----------------------------
+DROP TABLE IF EXISTS `sp_seed`;
+CREATE TABLE `sp_seed` (
+  `id` int(11) NOT NULL AUTO_INCREMENT COMMENT '自增id',
+  `name` varchar(255) NOT NULL COMMENT '名称',
+  `url` varchar(255) NOT NULL COMMENT 'url地址',
+  `create_uid` int(11) NOT NULL COMMENT '创建人id',
+  `create_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '创建时间',
+  `update_uid` int(11) DEFAULT NULL COMMENT '更新人id',
+  `update_time` timestamp NULL DEFAULT NULL COMMENT '更新时间',
+  PRIMARY KEY (`id`),
+  KEY `idx_name` (`name`),
+  KEY `idx_create_time` (`create_time`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Records of sp_seed
+-- ----------------------------
+
+-- ----------------------------
+-- Table structure for `sp_user`
+-- ----------------------------
+DROP TABLE IF EXISTS `sp_user`;
+CREATE TABLE `sp_user` (
+  `id` int(11) NOT NULL AUTO_INCREMENT COMMENT '自增id',
+  `username` varchar(30) NOT NULL COMMENT '用户名',
+  `password` varchar(30) NOT NULL COMMENT '密码(md5后的值)',
+  `realname` varchar(20) DEFAULT NULL COMMENT '真名',
+  `email` varchar(30) NOT NULL COMMENT '邮箱',
+  `sex` tinyint(4) NOT NULL COMMENT '1-male, 2-female',
+  `create_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '创建时间',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Records of sp_user
+-- ----------------------------
+INSERT INTO `sp_user` VALUES ('1', 'lbb', 'bb', '陆炳彬', 'lubingbin@qq.com', '1', '2017-04-22 23:08:55');
